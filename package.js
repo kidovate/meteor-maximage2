f7020c73c4c145f1131d71eb4e6cbc36207e3cc9
Christian Stewart <kidovate@gmail.com>
2014-05-26 11:27:15 -0700

    Initial

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  summary: "REPLACEME - What does this package (or the original one you're wrapping) do?"
+});
+
+Package.on_use(function (api, where) {
+  api.use(['cycle', 'jquery', 'bootstrap-3'], 'client');
+  api.add_files(
+    ['lib/js/jquery.maximage.js', 
+      ''
+      'lib/css/jquery.maximage.css'], 'client');
+});
